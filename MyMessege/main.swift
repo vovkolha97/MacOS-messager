4a7ef1762eac636b9eabe1d82cc7eed39282f032
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-02-05 14:33:59 +0200

    Initial Commit

---
+++ MyMessege/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyMessege
+//
+//  Created by Вовк Ольга' on 2/5/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
